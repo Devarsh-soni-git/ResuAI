731db930be4bd95c3d938e0640c36d3378dfc30c
Devarsh-soni-git <devarshsoni365@gmail.com>
2026-03-26 14:17:19 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://fkqpmbducnlvxazfgyra.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrcXBtYmR1Y25sdnhhemZneXJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwOTUwNTUsImV4cCI6MjA4NDY3MTA1NX0.WyRa2QxnO1DnTg3G0dcaNuPmCgXeOn_yUAmm1007BJI";
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_PUBLISHABLE_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
